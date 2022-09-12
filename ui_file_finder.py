5eccb72baf8ec5cef923105d9b3ad723fd1d3e4a
haksuman <51818946+haksuman@users.noreply.github.com>
2022-09-12 21:09:15 +0300

    background color update

--- ui_file_finder.py
+++ ui_file_finder.py
@@ -16,34 +16,40 @@ tab_control.add(tab2, text="File Lister")
 tab_control.pack(expand = 1, fill ="both")
 
 
+
 # Create Canvas
-canvas_tab1 = tk.Canvas(tab1, width=650, height=450)
+canvas_tab1 = tk.Canvas(tab1, width=650, height=450, background="lightblue")
 canvas_tab1.pack()
 
-canvas_tab2 = tk.Canvas(tab2, width=650, height=450)
+canvas_tab2 = tk.Canvas(tab2, width=650, height=450, background="lightblue")
 canvas_tab2.pack()
 
 
 # Tab1
 # Add label boxes
-tk.Label(canvas_tab1, text="Please input list file path").place(x=140, y=150)
-tk.Label(canvas_tab1, text="Please input source folder path").place(x=140, y=200)
-tk.Label(canvas_tab1, text="Please input target folder path").place(x=140, y=250)
-tk.Label(canvas_tab1, text="Please input file extension").place(x=140, y=300)
+tk.Label(canvas_tab1, text="Please input list file path", background="skyblue").place(x=100, y=150)
+tk.Label(canvas_tab1, text="Please input source folder path", background="skyblue").place(x=100, y=200)
+tk.Label(canvas_tab1, text="Please input target folder path", background="skyblue").place(x=100, y=250)
+tk.Label(canvas_tab1, text="Please input file extension", background="skyblue").place(x=100, y=300)
 
 
 # Add entry boxes
 path_src_list = tk.Entry(canvas_tab1)  # input box responsible for list of Excel file path
-canvas_tab1.create_window(400, 160, window=path_src_list)
+canvas_tab1.create_window(350, 160, window=path_src_list)
 
 path_src_folder = tk.Entry(canvas_tab1)  # input box responsible for source folder path
-canvas_tab1.create_window(400, 210, window=path_src_folder)
+canvas_tab1.create_window(350, 210, window=path_src_folder)
 
 path_target_folder = tk.Entry(canvas_tab1)  # input box responsible for target folder path
-canvas_tab1.create_window(400, 260, window=path_target_folder)
+canvas_tab1.create_window(350, 260, window=path_target_folder)
 
 txt_file_ext = tk.Entry(canvas_tab1)  # input box responsible for target folder path
-canvas_tab1.create_window(400, 310, window=txt_file_ext)
+canvas_tab1.create_window(350, 310, window=txt_file_ext)
+
+# Browse buttons
+btn_browse_src_list = tk.Button(text="Browse").place(x=440, y=170) # command declaration required for buttons
+btn_browse_src_folder = tk.Button(text="Browse").place(x=440, y=220)
+btn_target_folder = tk.Button(text="Browse").place(x=440, y=270)
 
 
 # include function
