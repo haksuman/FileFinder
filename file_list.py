17090b3f1c094c8628eec891c6cbbfcf007db642
Hakan AKSUMAN <h.aksuman@gmail.com>
2022-09-27 17:53:07 +0300

    file list tab2 update

--- file_list.py
+++ file_list.py
@@ -2,14 +2,15 @@ import os
 import pandas as pd
 # this script stands for files in specific folder to be listed
 
-def list_files(src_path, dst_path):
+def list_files(src_path, output_file_name):
     list_element = os.listdir(src_path)
     df = pd.DataFrame()
     df['file_name'] = list_element
+    dst_path = src_path + "\\" + output_file_name + ".xlsx"    
     df.to_excel(dst_path)
 
-if __name__ == '__main__':
-    pth = r'\\fs-awp\01_TECHNICAL_OFFICE\TECH_OFFICE_PP_QS\002-PROGRESS PAYMENT\000_Follow Up tables\3620-STEEL STRUCTURE\GRATING\Transport Nakladnaya' # folder path that needs to be listed
-    dst_path = r'\\fs-awp\01_TECHNICAL_OFFICE\TECH_OFFICE_PP_QS\002-PROGRESS PAYMENT\000_Follow Up tables\3620-STEEL STRUCTURE\GRATING\file_list.xlsx'
+#if __name__ == '__main__':
+#    pth = r'\\fs-awp\01_TECHNICAL_OFFICE\TECH_OFFICE_PP_QS\002-PROGRESS PAYMENT\000_Follow Up tables\3620-STEEL STRUCTURE\GRATING\Transport Nakladnaya' # folder path that needs to be listed
+#    dst_path = r'\\fs-awp\01_TECHNICAL_OFFICE\TECH_OFFICE_PP_QS\002-PROGRESS PAYMENT\000_Follow Up tables\3620-STEEL STRUCTURE\GRATING\file_list.xlsx'
 
 
--- ui_file_finder.py
+++ ui_file_finder.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 from file_finder import file_copy_func
+from file_list import list_files
 
 
 root = tk.Tk()
@@ -18,10 +20,10 @@ tab_control.pack(expand = 1, fill ="both")
 
 
 # Create Canvas
-canvas_tab1 = tk.Canvas(tab1, width=650, height=450, background="lightblue")
+canvas_tab1 = tk.Canvas(tab1, width=550, height=450, background="lightblue")
 canvas_tab1.pack()
 
-canvas_tab2 = tk.Canvas(tab2, width=650, height=450, background="lightblue")
+canvas_tab2 = tk.Canvas(tab2, width=550, height=450, background="lightblue")
 canvas_tab2.pack()
 
 
@@ -31,6 +33,8 @@ tk.Label(canvas_tab1, text="Please input list file path", background="skyblue").
 tk.Label(canvas_tab1, text="Please input source folder path", background="skyblue").place(x=100, y=200)
 tk.Label(canvas_tab1, text="Please input target folder path", background="skyblue").place(x=100, y=250)
 tk.Label(canvas_tab1, text="Please input file extension", background="skyblue").place(x=100, y=300)
+# Copyright
+tk.Label(canvas_tab1, text="Created by Haksuman", background="lightblue").place(x=205, y=400)
 
 
 # Add entry boxes
@@ -47,9 +51,9 @@ txt_file_ext = tk.Entry(canvas_tab1)  # input box responsible for target folder 
 canvas_tab1.create_window(350, 310, window=txt_file_ext)
 
 # Browse buttons
-btn_browse_src_list = tk.Button(text="Browse").place(x=440, y=170) # command declaration required for buttons
-btn_browse_src_folder = tk.Button(text="Browse").place(x=440, y=220)
-btn_target_folder = tk.Button(text="Browse").place(x=440, y=270)
+#btn_browse_src_list = tk.Button(canvas_tab1, text="Browse").place(x=440, y=150) # command declaration required for buttons
+#btn_browse_src_folder = tk.Button(canvas_tab1, text="Browse").place(x=440, y=220)
+#btn_target_folder = tk.Button(canvas_tab1, text="Browse").place(x=440, y=270)
 
 
 # include function
@@ -60,14 +64,42 @@ def fnc_btn_copy_files():
     input_file_ext = str(txt_file_ext.get())
 
     file_copy_func(input_path_src_list, input_path_src_folder, input_path_target_folder, input_file_ext)  # calling back function from file_finder.py
+    tk.messagebox.showinfo(title="Status Notification", message="Completed!")
 
 
 # add function button
 btn_copy_files = tk.Button(text='Copy files to target location', command=fnc_btn_copy_files)
-canvas_tab1.create_window(300, 350, window=btn_copy_files)
+canvas_tab1.create_window(270, 350, window=btn_copy_files)
 
 
 # Tab2
+# Labels for tab2
+tk.Label(canvas_tab2, text="Please input source folder path", background="skyblue").place(x=100, y=150)
+tk.Label(canvas_tab2, text="Please input file name for output", background="skyblue").place(x=100, y=200)
+# Copyright
+tk.Label(canvas_tab2, text="Created by Haksuman", background="lightblue").place(x=205, y=400)
+
+#Entris for tab2
+path_src_folder_to_list = tk.Entry(canvas_tab2)  # input box responsible for list of Excel file path
+canvas_tab2.create_window(350, 160, window=path_src_folder_to_list )
+
+name_file_list_output = tk.Entry(canvas_tab2)  # input box responsible for source folder path
+canvas_tab2.create_window(350, 210, window=name_file_list_output)
+
+# list files function
+def fnc_btn_list_files():
+    input_path_src_folder_to_list = path_src_folder_to_list.get()
+    input_name_file_list_output = name_file_list_output.get() 
+    list_files(input_path_src_folder_to_list, input_name_file_list_output)  # calling back function from file_finder.py
+    tk.messagebox.showinfo(title="Status Notification", message="Completed!")
+
+# Action button to copy files
+btn_list_files = tk.Button(canvas_tab2, text='List files in target location', command=fnc_btn_list_files)
+canvas_tab2.create_window(280, 250, window=btn_list_files)
+
+
+
+
 
 
 root.mainloop()
